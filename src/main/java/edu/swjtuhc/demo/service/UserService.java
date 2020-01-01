139008b9d50320894997c8bd6459dc5ae3989f7a
Liangxirui <1362589207@qq.com>
2020-01-02 00:08:20 +0800

    12.08

--- src/main/java/edu/swjtuhc/demo/service/UserService.java
+++ src/main/java/edu/swjtuhc/demo/service/UserService.java
@@ -4,6 +4,6 @@ import edu.swjtuhc.demo.model.SysUser;
 
 public interface UserService {
 		public int register(SysUser user);
-		 public SysUser userLogin(String username);
-		SysUser selectUser(String username);
+		 public SysUser userLogin(String u_name);
+		SysUser selectUser(String u_name);
 }
